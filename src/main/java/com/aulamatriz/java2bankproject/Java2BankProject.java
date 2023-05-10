4e4bbd0460b74f10f49c99f07431b8239404dcac
Edwin Porras A <edwin.porras@gmail.com>
2023-05-10 12:10:01 -0500

    Carga Inicial

---
+++ src/main/java/com/aulamatriz/java2bankproject/Java2BankProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aulamatriz.java2bankproject;
+
+/**
+ *
+ * @author edwin
+ */
+public class Java2BankProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
